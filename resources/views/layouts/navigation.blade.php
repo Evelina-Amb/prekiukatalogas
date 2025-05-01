5a78fd9e6ec285867654ee28740ac17cbd3ec3cd
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2025-05-01 06:42:06 -0700

    Add files via upload
    pataisyta nuoroda i kataloga

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,7 +10,7 @@
                     </x-nav-link>
                 </div>
 				<div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('katalogas')" :active="request()->routeIs('katalogas')">
                         {{ __('katalogas') }}
                     </x-nav-link>
                 </div>
