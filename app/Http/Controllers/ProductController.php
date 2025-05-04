f0c1e6414d083d4e1edfb957ee38fbaf40992992
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2025-05-04 11:01:07 -0700

    filtrai

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,13 +4,48 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\City;
+use App\Models\Category;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $products = Product::with(['category', 'company.city'])->get();
+        $query = Product::with(['category', 'company.city']);
 
-        return view('katalogas', compact('products'));
+if ($request->filled('cities')) {
+    $query->whereHas('company', function ($q) use ($request) {
+        $q->whereIn('city_id', $request->cities);
+    });
+}
+
+
+        if ($request->filled('quantity_min')) {
+            $query->where('quantity', '>=', $request->quantity_min);
+        }
+
+        if ($request->filled('quantity_max')) {
+            $query->where('quantity', '<=', $request->quantity_max);
+        }
+
+        if ($request->filled('price_min')) {
+            $query->where('price', '>=', $request->price_min);
+        }
+
+        if ($request->filled('price_max')) {
+            $query->where('price', '<=', $request->price_max);
+        }
+
+        if ($request->filled('categories') && !in_array(null, $request->categories)) {
+    $query->whereIn('category_id', $request->categories);
+}
+
+        $products = $query->get();
+
+        return view('katalogas', [
+            'products' => $products,
+            'cities' => City::all(),
+            'categories' => Category::all(),
+        ]);
     }
 }
