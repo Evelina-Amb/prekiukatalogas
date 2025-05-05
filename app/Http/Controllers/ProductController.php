3c9d078b948ce678d8befd44b425cb74507469d3
Evelina-Amb <evelina.ambrozaite@stud.svako.lt>
2025-05-05 07:11:54 -0700

    pdf

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\City;
 use App\Models\Category;
+use PDF;
 
 class ProductController extends Controller
 {
@@ -48,4 +49,43 @@ if ($request->filled('cities')) {
             'categories' => Category::all(),
         ]);
     }
+	
+public function exportPdf(Request $request)
+{
+    $query = Product::with(['category', 'company.city']);
+
+    if ($request->filled('cities')) {
+        $cities = collect($request->input('cities'))->filter()->map(fn($v) => (int) $v)->all();
+        if (!empty($cities)) {
+            $query->whereHas('company', fn($q) => $q->whereIn('city_id', $cities));
+        }
+    }
+
+    if ($request->filled('categories')) {
+        $categories = collect($request->input('categories'))->filter()->map(fn($v) => (int) $v)->all();
+        if (!empty($categories)) {
+            $query->whereIn('category_id', $categories);
+        }
+    }
+
+    if ($request->filled('quantity_min')) {
+        $query->where('quantity', '>=', (int) $request->input('quantity_min'));
+    }
+
+    if ($request->filled('quantity_max')) {
+        $query->where('quantity', '<=', (int) $request->input('quantity_max'));
+    }
+
+    if ($request->filled('price_min')) {
+        $query->where('price', '>=', (float) $request->input('price_min'));
+    }
+
+    if ($request->filled('price_max')) {
+        $query->where('price', '<=', (float) $request->input('price_max'));
+    }
+
+    $products = $query->get();
+    $pdf = PDF::loadView('pdf.katalogas', ['products' => $products]);
+    return $pdf->download('produktai.pdf');
+}
 }
